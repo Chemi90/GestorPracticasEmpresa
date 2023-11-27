451d76f7a5b940951167626090009b03582abf73
Jose Miguel <xemiruiz@hotmail.com>
2023-11-27 10:20:19 +0100

    editarEmpresa-view.fxml hecho y maquetado con su respectivo controller

---
+++ GestorPracticasEmpresa/src/main/java/com/example/gestorpracticasempresa/fichaEmpresaController.java
@@ -0,0 +1,29 @@
+package com.example.gestorpracticasempresa;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class fichaEmpresaController {
+
+    @FXML
+    private ImageView icnPersona;
+    @FXML
+    private Button btnSalir;
+    @FXML
+    private Label lbNombreEmpresa;
+    @FXML
+    private Label lbTelefono;
+    @FXML
+    private Label lbEmail;
+    @FXML
+    private Label lbResponsable;
+    @FXML
+    private Label lbObservaciones;
+    @FXML
+    private Button btnVolver;
+    @FXML
+    private Button btnEditar;
+
+}
