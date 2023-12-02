8ab93f66cec305d0e590803d4807a03855507b1a
Overion <pablopostigomorales@gmail.com>
2023-12-02 20:09:01 +0100

    adding Profesor init file

---
+++ GestorPracticasEmpresa/src/main/java/com/example/gestorpracticasempresa/controllers/IniciarProfesorController.java
@@ -0,0 +1,16 @@
+package com.example.gestorpracticasempresa.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
+public class IniciarProfesorController {
+    @FXML
+    private TableColumn tcNombreAlumno;
+    @FXML
+    private TableColumn tcEntradas;
+    @FXML
+    private TableColumn tcNombreEmpresa;
+    @FXML
+    private Label lblNombreProf;
+}
