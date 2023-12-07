4ef84b70c1bcdd30520265d015ab014627aa16d9
Overion <pablopostigomorales@gmail.com>
2023-12-07 18:16:12 +0100

    adding crearAlumno and crearEmpresa

---
+++ GestorPracticasEmpresa/src/main/java/com/example/gestorpracticasempresa/controllers/CrearAlumnoController.java
@@ -0,0 +1,36 @@
+package com.example.gestorpracticasempresa.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class CrearAlumnoController {
+    @FXML
+    private TextField txtNombreAlum;
+    @FXML
+    private TextField txtTlfAlum;
+    @FXML
+    private TextField txtDniAlum;
+    @FXML
+    private PasswordField txtPassAlum;
+    @FXML
+    private ComboBox cbEmpAlum;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Button btnCrearEmp;
+    @FXML
+    private TextField txtEmailAlum;
+    @FXML
+    private ComboBox cbPractAlum;
+    @FXML
+    private Spinner spHorasAlum;
+    @FXML
+    private DatePicker dpFechaNacAlum;
+    @FXML
+    private ComboBox cbTutorAlum;
+}
---
+++ GestorPracticasEmpresa/src/main/java/com/example/gestorpracticasempresa/controllers/CrearEmpresaController.java
@@ -0,0 +1,18 @@
+package com.example.gestorpracticasempresa.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CrearEmpresaController {
+    @FXML
+    private TextField txtResponEmp;
+    @FXML
+    private TextField txtNombreEmp;
+    @FXML
+    private TextField txtTlfEmp;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Button btnCrearEmp;
+}
