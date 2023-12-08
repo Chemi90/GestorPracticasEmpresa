9e320c7df124a50324a908721cf5e9a68f6c5d7a
aaleaalvarez <alejandro.s297816@cesurformacion.com>
2023-12-08 13:14:00 +0100

    Nuevos fxml con sus controladores

---
+++ GestorPracticasEmpresa/src/main/java/com/example/gestorpracticasempresa/controllers/AlumnoTablaController.java
@@ -0,0 +1,24 @@
+package com.example.gestorpracticasempresa.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class AlumnoTablaController
+{
+    @javafx.fxml.FXML
+    private Button salirBtn;
+    @javafx.fxml.FXML
+    private Button volverBtn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void salir(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void volver(ActionEvent actionEvent) {
+    }
+}
